dd000ce878e5659ce1218377c0d6f7a46e76f531
afonsomartins <afonsomartins@ff18763b-def8-59af-a475-44e6d855d1a2>
2010-05-29 17:23:05 +0000

    first commit

---
+++ skin/frontend/default/default/js/zoomer-min.js
@@ -0,0 +1,25 @@
+/*
+Copyright (c) 2009 Victor Stanciu - http://www.victorstanciu.ro
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('R=S.T({E:m(a,b,c){2.3=$(a);2.f=2.3.U(\'V\');2.7={4:2.f.i,g:2.3.W}j(!c){s d=n F();d.i=2.7.4;d.G=2.E.t(2).X(a,b,H);Y}2.o=2.7.4;2.9=Z.10({u:v,w:v,z:v},b||{});2.A=2.3.11();2.8={4:{6:2.f.12(),e:2.f.13()}}2.p=1;2.k=n F();2.k.i=2.7.g;2.k.G=2.q.t(2);2.l=(n I(\'J\')).K({L:\'14\',6:2.8.4.6+M+\'r\',e:2.8.4.e+M+\'r\'});2.3.N(2.l);2.O=(n I(\'J\').K({L:\'15\',6:2.8.4.6+\'r\',e:2.8.4.e+\'r\'}));2.l.N(2.O);j(2.9.u){$(2.9.u).B(\'h\',2.h.C(2))}2.3.B(\'h\',2.h.C(2));2.3.B(\'16\',2.D.C(2))},h:m(a){a.17();18(2.o){P 2.7.4:j(2.3.q){2.f.i=2.o=2.7.g;2.3.19();2.D(a);j(2.9.w){2.9.w()}}1a{2.3.1b();2.h.t(2).1c(0.5,a)}Q;P 2.7.g:2.f.i=2.o=2.7.4;j(2.9.z){2.9.z()}Q}},D:m(a){s x=a.1d()-2.A.1e;s y=a.1f()-2.A.1g;2.l.1h=x*2.p-x;2.l.1i=y*2.p-y},q:m(){2.3.q=H;2.8.g={6:2.k.6,e:2.k.e};2.p=2.8.g.6/2.8.4.6}});',62,81,'||this|element|small||width|source|dimensions|options|||||height|image|large|click|src|if|preload|scroller|function|new|selected|scaleReport|loaded|px|var|bind|trigger|null|afterZoomIn|||afterZoomOut|position|observe|bindAsEventListener|move|initialize|Image|onload|true|Element|div|setStyle|overflow|20|wrap|wrapper|case|break|Zoomer|Class|create|down|img|href|curry|return|Object|extend|cumulativeOffset|getWidth|getHeight|auto|hidden|mousemove|stop|switch|show|else|hide|delay|pointerX|left|pointerY|top|scrollLeft|scrollTop'.split('|'),0,{}))
